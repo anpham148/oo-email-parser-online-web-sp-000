7887c1c1a1095b67f9551aa47509620f45263a70
An Pham <anpham148@gmail.com>
2020-07-07 08:49:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   end
 
   def parse
-     @email_addresses.split(/[ ,]/)
+     result = @email_addresses.split(/[ ,]/)
     binding.pry
      result.filter {|x| x != " "}
      result
